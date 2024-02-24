ebcfd7c883207a959f473102dfccf644525a1064
Omar gamal mohammed <155237219+omargamal1234-glitch@users.noreply.github.com>
2024-02-24 00:35:55 -0800

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,33 +6,49 @@ const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
-	cors: {
-		origin: ["http://localhost:3000"],
-		methods: ["GET", "POST"],
-	},
+  cors: {
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+  },
 });
 
 export const getReceiverSocketId = (receiverId) => {
-	return userSocketMap[receiverId];
+  return userSocketMap[receiverId];
 };
 
 const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
-	console.log("a user connected", socket.id);
+  console.log("a user connected", socket.id);
 
-	const userId = socket.handshake.query.userId;
-	if (userId != "undefined") userSocketMap[userId] = socket.id;
+  const userId = socket.handshake.query.userId;
+  if (userId != "undefined") userSocketMap[userId] = socket.id;
 
-	// io.emit() is used to send events to all the connected clients
-	io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  // Emit online users to everyone
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-	// socket.on() is used to listen to the events. can be used both on client and server side
-	socket.on("disconnect", () => {
-		console.log("user disconnected", socket.id);
-		delete userSocketMap[userId];
-		io.emit("getOnlineUsers", Object.keys(userSocketMap));
-	});
+  // Handle disconnections
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+
+  // Handle message sending
+  socket.on("sendMessage", (data) => {
+    const { senderId, receiverId, message } = data;
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+
+    if (receiverSocketId) {
+      // Send the message to the receiver's socket
+      io.to(receiverSocketId).emit("receiveMessage", { senderId, message });
+    } else {
+      // Handle the case where the receiver is not online
+      console.log("Receiver not online:", receiverId);
+      // You can send a notification to the sender, store the message for later delivery, etc.
+    }
+  });
 });
 
 export { app, io, server };
